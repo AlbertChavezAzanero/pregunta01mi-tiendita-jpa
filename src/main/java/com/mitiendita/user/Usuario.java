014fd3afcd277610d1cd2b27f8c9e08910da4be0
Albert Gemison Chavez Azañero <agchaveza2000@gmail.com>
2025-08-03 23:26:59 -0500

    Update Usuario.java

--- src/main/java/com/mitiendita/user/Usuario.java
+++ src/main/java/com/mitiendita/user/Usuario.java
@@ -4,10 +4,9 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Data // Anotación de Lombok para generar getters, setters, toString, etc.
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // Estrategia de Herencia
+@Data 
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) 
 @DiscriminatorColumn(name = "TIPO_USUARIO", discriminatorType = DiscriminatorType.STRING)
-// Columna que diferenciará el tipo de usuario (Admin o Cliente)
 public abstract class Usuario {
 
     @Id
